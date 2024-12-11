18a2025e5dbabe070d36e2852330135938a9b303
BangKartavya <bangkartavya@gmail.com>
2024-12-12 02:28:45 +0530

    undefined behaviour fixed

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -14,7 +14,7 @@ const Thumbnail = ({ type, extension, url, imageClassName, className }: Props) =
     const isImage = type === "image" && extension !== 'svg';
     return <figure className={cn('thumbnail', className)}>
         <Image
-            src={isImage ? url : getFileIcon(extension, type)}
+            src={isImage && url ? url : getFileIcon(extension, type)}
             alt="thumbnail"
             width={100}
             height={100}
