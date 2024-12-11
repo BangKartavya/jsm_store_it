a0ac87ef790352fc3e72cce9488d6bcb7dec97b4
BangKartavya <bangkartavya@gmail.com>
2024-12-12 02:41:53 +0530

    homepage bug

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -4,6 +4,7 @@ import { appwriteConfig } from "./config"
 import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
+
     const client = new Client().setEndpoint(appwriteConfig.endpointUrl).setProject(appwriteConfig.projectId);
     const session = (await cookies()).get('appwrite-session');
 
